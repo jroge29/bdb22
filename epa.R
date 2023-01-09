635ee59d894b51e6de7ee25ee4793c2a011a3a1c
IsaacBlumSchool <121645190+IsaacBlumSchool@users.noreply.github.com>
2023-01-09 00:02:39 -0600

    Add files via upload

--- epa.R
+++ epa.R
@@ -22,10 +22,43 @@ write.csv(with_epa, "with_epa.csv")
 
 
 
+
+
 finalplays$gameId <- as.character(finalplays$gameId)
 finalplays$playId <- as.character(finalplays$playId)
 
 change <- left_join(finalplays, front[c(2,3,6)], by = c("gameId", "playId"))
 change2 <- unique(change)
 
+change3 <- filter(change2, ((`('stunt_class', '')` == 2) & (pff_playAction == 0) & (`('blitz_class', '')` %in% (c(0,2,3)))) | ((`('stunt_class', '')` == 4) & (pff_playAction == 0) & (`('blitz_class', '')` == 0)))
 
+change3$gameId <- as.numeric(change3$gameId)
+change3$playId <- as.numeric(change3$playId)
+
+change4 <- left_join(change3, with_epa[c(2,3,38)], by = c("gameId", "playId"))
+
+change5 <- filter(change4, `('stunt_class', '').y` == 0)
+
+
+for (i in 1:nrow(change5)){
+  if (change5$playDirection[i] == 'left'){
+    if (change5$`('stunt_class', '').x`[i] == 2){
+      change5$`('stunt_class', '').x`[i] = 4
+    }
+    else{
+      change5$`('stunt_class', '').x`[i] = 2
+    }
+  }
+  i = i + 1
+}
+
+with_epa2 <- left_join(with_epa, change5[c(2,3,37)], by = c("gameId", "playId"))
+
+with_epa2 <- mutate(with_epa2, `('stunt_class', '')` = ifelse(!is.na(`('stunt_class', '').x`), `('stunt_class', '').x`, `('stunt_class', '')`))
+with_epa2 <- select(with_epa2, -c(41))
+
+with_epa <- with_epa2
+
+
+
+write.csv(with_epa, "with_epa.csv")
